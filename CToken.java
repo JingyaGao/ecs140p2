0da3bd19efbb12dda997087eae8c1f8dd84f95cb
tbisht12 <tbisht@ucdavis.edu>
2017-04-23 16:54:49 -0700

    Create CToken.java

---
+++ CToken.java
@@ -0,0 +1,13 @@
+public class CToken {
+    public int linenum = 0;
+    public int charnum = 0;
+    public String type = "";
+    public String token = "";
+    //constructor
+    public Point(int l, int c, String tokenType, String tokenName) {
+        linenum = a;
+        charnum = b;
+        type = tokenType;
+        token = tokenName;
+    }
+}
