1eaee03ebce6c495f094c4867cb089f85752f786
tbisht12 <tbisht@ucdavis.edu>
2017-04-23 15:34:31 -0700

    Create Tokenizer.java

---
+++ Tokenizer.java
@@ -0,0 +1,13 @@
+public class Tokenizer{
+    public static void main(String[] args){
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(
+                new FileInputStream(file),
+                Charset.forName("UTF-8")));
+        int c;
+        while((c = reader.read()) != -1) {
+          char character = (char) c;
+          System.out.println(character);
+        }
+    }
+}
